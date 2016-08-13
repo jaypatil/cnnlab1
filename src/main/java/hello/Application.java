89caeb6f79246c21529a1a52e95c15f6fb92a15f
Jay Kumar Patil <jaypatil.sonu@gmail.com>
2016-08-13 17:53:13 +0530

    First Commit

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,16 @@
+package hello;
+
+/**
+ * Created by Asha on 13-Aug-16.
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
